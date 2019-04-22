2c9747b7271d470ec1fcdfab7ee82ee7aa849011
Farid Saud <faridsaud93@gmail.com>
2019-04-22 15:51:56 -0500

    Feat: Add option to set base values

--- src/dimensions.js
+++ src/dimensions.js
@@ -1,4 +1,9 @@
 
+const baseValues = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 /**
  * Initial Sketch File values, represent the max width and height of an screen in the sketch file.
  * @type {{height: number, width: number}}
@@ -12,10 +17,17 @@ const sketch = {
  * Init the sketch object
  * @param width number of pixels of a sketch's screen width
  * @param height height of pixels of a sketch's screen height
+ * @param baseWidth width in pixels used as 100%
+ * @param baseHeight height in pixels used as 100%
  */
-export const init = ({ width, height }) => {
+export const init = ({
+  width, height,
+  baseWidth = window.innerWidth, baseHeight = window.innerHeight,
+}) => {
   sketch.width = width;
   sketch.height = height;
+  baseValues.width = baseWidth;
+  baseValues.height = baseHeight;
 };
 
 /**
@@ -26,7 +38,7 @@ export const init = ({ width, height }) => {
  */
 export const getWidthValue = (widthValueInSketch, {
   width,
-} = sketch) => widthValueInSketch / width * window.innerWidth;
+} = sketch) => widthValueInSketch / width * baseValues.width;
 
 /**
  * Get the actual number of pixels used for height dimensions
@@ -36,7 +48,7 @@ export const getWidthValue = (widthValueInSketch, {
  */
 export const getHeightValue = (heightValueInSketch, {
   height,
-} = sketch) => heightValueInSketch / height * window.innerHeight;
+} = sketch) => heightValueInSketch / height * baseValues.height;
 
 
 export default {
