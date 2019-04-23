cd3febf847b22a94e2191d7475b776612f35a5ac
Farid Saud <faridsaud93@gmail.com>
2019-04-23 13:48:21 -0500

    Feat: Update build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+
 const path = require('path');
+const webpack = require('webpack');
 
-
-module.exports = {
+const webpack_rules = [];
+const webpackOption = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -9,4 +11,19 @@ module.exports = {
     library: 'responsiveDimensionsWeb',
     libraryTarget: 'umd',
   },
+  module: {
+    rules: webpack_rules,
+  },
 };
+const babelLoader = {
+  test: /\.js$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env'],
+    },
+  },
+};
+webpack_rules.push(babelLoader);
+module.exports = webpackOption;
